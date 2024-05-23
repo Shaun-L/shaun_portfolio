076ea5691d80defdc0c193495ff14a2ea5c993bb
Shaun Lim <shaunlim1214@gmail.com>
2024-05-23 14:50:00 -0700

    About Section finished

--- shaun-portfolio/src/components/About/About.jsx
+++ shaun-portfolio/src/components/About/About.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
+import styles from "./About.module.css"
 
 export const about = () => {
-  return <section>
-    <h2>About</h2>
-    <div>
+  return <section className={styles.container} id="about">
+    <h2 className={styles.title}>About</h2>
+    <div className={styles.content}>
       <img src ={getImageUrl("about/aboutImage.png")}
-      alt ="Me sitting with a laptop"/>
-      <ul>
-        <li>
+      alt ="Me sitting with a laptop"
+      className={styles.aboutImage}/>
+      <ul className={styles.aboutItems}>
+        <li className={styles.aboutItem}>
           <img src={getImageUrl("about/cursorIcon.png")} 
-          alt="Cursor"/>
-          <div>
+          alt="Cursor Icon"/>
+          <div className={styles.aboutItemText}>
             <h3>Software developer</h3>
+            <p>I'm a software developer with experienc in building *finish this please* </p>
+          </div>
+        </li>
+        <li className={styles.aboutItem}>
+          <img src={getImageUrl("about/serverIcon.png")} 
+          alt="Server Icon"/>
+          <div className={styles.aboutItemText}>
+            <h3>*Change This*</h3>
+            <p>I'm a software developer with experienc in building "finish this please" </p>
+          </div>
+        </li>
+        <li className={styles.aboutItem}>
+          <img src={getImageUrl("about/cursorIcon.png")} 
+          alt="UI Icon"/>
+          <div className={styles.aboutItemText}>
+            <h3>*Change This*</h3>
             <p>I'm a software developer with experienc in building "finish this please" </p>
           </div>
         </li>
